76e58e55c1799cd141628dc3b307899badfdb117
peteriguanya <iguanyapeter7@gmail.com>
2025-12-03 15:01:31 +0300

    Fix: display_balance prints two decimals

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -17,7 +17,7 @@ class BankAccount:
 
     def display_balance(self):
         """Print the current balance."""
-        print(f"Current Balance: ${self.account_balance}")
+        print(f"Current Balance: ${self.account_balance:.2f}")
 
 
 
