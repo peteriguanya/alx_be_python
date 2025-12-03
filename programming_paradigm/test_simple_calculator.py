bea2e393377ff19af13fe6697f3f63d5c133630e
peteriguanya <iguanyapeter7@gmail.com>
2025-12-03 15:08:00 +0300

    Add/update files for tasks 1-4: division calculator, calculator tests, library management

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,30 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+
+    def setUp(self):
+        self.calc = SimpleCalculator()
+
+    def test_add(self):
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(0, 0), 0)
+
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(5, 3), 2)
+        self.assertEqual(self.calc.subtract(0, 5), -5)
+        self.assertEqual(self.calc.subtract(-3, -3), 0)
+
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(2, 3), 6)
+        self.assertEqual(self.calc.multiply(-1, 5), -5)
+        self.assertEqual(self.calc.multiply(0, 100), 0)
+
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(10, 2), 5)
+        self.assertEqual(self.calc.divide(3, 2), 1.5)
+        self.assertIsNone(self.calc.divide(10, 0))
+
+if __name__ == "__main__":
+    unittest.main()
